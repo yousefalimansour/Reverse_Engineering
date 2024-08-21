535826dd598ab6568d44a1d12ebd2cb0cdcd11cb
Yousef Ali <you@example.com>
2024-08-21 20:29:15 +0300

    Add project files.

---
+++ Reverse_Engineering/Program.cs
@@ -0,0 +1,16 @@
+﻿using Reverse_Engineering.Data;
+
+namespace Reverse_Engineering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var contxt in new AppDBContext().Speakers)
+            {
+                Console.WriteLine($"{contxt.FirstName} {contxt.LastName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reverse_EngineeringNETCLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reverse_EngineeringNETCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
